dae5182a40abb96442e3c0b5669fe7174e6e09f6
Alberto Gila Navarro <60033875+AlbertoGilaNavarro@users.noreply.github.com>
2020-05-11 21:20:55 +0200

    Update SuperSorter.sh
    Mejorado SuperSorter para que sea mas facil de leer la salida.

--- SuperSorter.sh
+++ SuperSorter.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 file=$1
 index=4
-outfile="Out.txt"
+outfile="ChosenProteins.txt"
 for ((index=4; index<=8; index++)); do
+	name=$(cat ${file} | head -1 | cut -f ${index}
+	echo ${name} >> ${outfile}
 	sort ${file} -k ${index} -r | cut -f 1 | head -3 | tail -2 >> ${outfile}
 done
